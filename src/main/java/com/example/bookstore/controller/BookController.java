e85625b1ff77ae908b2a0a2bd782d7f4a71956f4
Ahmad Omar <eng.pctec@gmail.com>
2023-03-05 14:58:13 +0200

    Spring Security

--- src/main/java/com/example/bookstore/controller/BookController.java
+++ src/main/java/com/example/bookstore/controller/BookController.java
@@ -1,6 +1,8 @@
 package com.example.bookstore.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +10,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import com.example.bookstore.domain.Book;
 import com.example.bookstore.domain.BookRepository;
 import com.example.bookstore.domain.CategoryRepository;
@@ -19,25 +20,31 @@ public class BookController {
 
 	@Autowired
 	private BookRepository repository;
-	
 	@Autowired
-	private CategoryRepository catrepository;
+	private CategoryRepository catgoryRepository;
 
 	@GetMapping("/index")
 	public String book(Model model) {
 		return "index";
 	}
+	
+	@GetMapping(value="/login")
+	public String login(Model model) {
+		return "login";
+	}
 
 	@GetMapping("/booklist")
-	public String bookList(Model model) {
+	public String bookList(Model model, Authentication authentication) {
 		model.addAttribute("books", repository.findAll());
+		model.addAttribute("username", authentication.getName());
 		return "booklist";
 	}
 
 	@GetMapping("/add")
 	public String addBookForm(Model model) {
 		model.addAttribute("book", new Book());
-		model.addAttribute("categories", catrepository.findAll());
+		model.addAttribute("categories", catgoryRepository.findAll());
+
 		return "addbook";
 	}
 
@@ -47,40 +54,24 @@ public class BookController {
 		return "redirect:/booklist";
 	}
 
+	@PreAuthorize("hasRole('ADMIN')")
 	@GetMapping("/delete/{id}")
 	public String deleteBook(@PathVariable("id") Long id) {
 		repository.deleteById(id);
 		return "redirect:/booklist";
 	}
 
-	/*	@GetMapping("/edit/{id}")
-	public String editBook(@PathVariable("id") long id, @PathVariable("cid") long cid, Model model) {
-		Book book = repository.findById(id).get();
-		Category categories = catrepository.findById(cid).get();
-		model.addAttribute("book", book);
-		model.addAttribute("categories", categories);
+	@RequestMapping(value="/edit/{id}")
+	public String editBook(@PathVariable("id") Long id, Model model) {
+		model.addAttribute("book", repository.findById(id));
+		model.addAttribute("categories", catgoryRepository.findAll());
 		return "editbook";
 	}
 
-	@PostMapping("/edit/{id}")
-	public String updateBook(@PathVariable("id") long id, @ModelAttribute Book book) {
-		book.setId(id);
-		repository.save(book);
-		return "redirect:/booklist";
-	}
-	*/
-	
 	@PostMapping(value="/save")
 	public String save(Book book) {
 		repository.save(book);
 		return "redirect:booklist";
 	}
-	
-	@RequestMapping(value="/edit/{id}")
-	public String editBook(@PathVariable("id") Long id, Model model) {
-		model.addAttribute("book", repository.findById(id));
-		model.addAttribute("categories", catrepository.findAll());
-		return "editbook";
-	}	
 
 }
