71e7f69fba990481ea397391e615b66bbc3c0258
Ahmad Omar <eng.pctec@gmail.com>
2023-03-12 13:31:27 +0200

    Testing and Deployment

--- src/main/java/com/example/bookstore/domain/BookRepository.java
+++ src/main/java/com/example/bookstore/domain/BookRepository.java
@@ -10,4 +10,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 public interface BookRepository extends CrudRepository<Book, Long> {
 	List<Book> findByTitle(@Param("title") String title);
 
+	long deleteByTitle(String title);
+
 }
--- src/main/java/com/example/bookstore/domain/CategoryRepository.java
+++ src/main/java/com/example/bookstore/domain/CategoryRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
 
-	Category findByName(String string);
+	Category findByName(String name);
+
+	long deleteByName(String title);
 
 }
