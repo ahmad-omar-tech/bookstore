94240269c3202866b8308cf828fc5ea4e0d314db
Ahmad Omar <eng.pctec@gmail.com>
2023-04-01 13:54:23 +0300

    fixing websecutiry and redirection

--- src/main/java/com/example/bookstore/WebSecurityConfiguration.java
+++ src/main/java/com/example/bookstore/WebSecurityConfiguration.java
@@ -1,13 +1,18 @@
 package com.example.bookstore;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import com.example.bookstore.controller.UserDetailService;
+
+
+//import com.example.bookstore.controller.UserDetailService;
 
 @Configuration
 @EnableWebSecurity
@@ -19,10 +24,30 @@ public class WebSecurityConfiguration {
 		super();
 		this.userDetailService = userDetailService;
 	}
-	protected void configure(HttpSecurity http) throws Exception {
+    
+    
+    @Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    	http
+			.authorizeHttpRequests((requests) -> requests
+				.requestMatchers("/CSS/**").permitAll()
+				.anyRequest().authenticated()
+			)
+			.formLogin((form) -> form
+				.loginPage("/login")
+				.defaultSuccessUrl("/booklist")
+				.permitAll()
+			)
+			.logout((logout) -> logout
+					.logoutSuccessUrl("/login")
+					.permitAll());
+
+		return http.build();
+	}
+	/*protected void configure(HttpSecurity http) throws Exception {
         http
         .authorizeHttpRequests()
-            .requestMatchers("/CSS/**").permitAll()
+            .requestMatchers("/login").permitAll()
             .and()
         .formLogin()
             .loginPage("/login")
@@ -33,7 +58,7 @@ public class WebSecurityConfiguration {
         	.logoutSuccessUrl("/login")
             .permitAll();
         
-    }
+    }*/
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailService).passwordEncoder(new BCryptPasswordEncoder());
